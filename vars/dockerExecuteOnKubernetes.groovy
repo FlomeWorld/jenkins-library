e7d84185971ab15427674cb53e39178b78e6bebc
Ramachandra Kamath Arbettu <rkamath3@users.noreply.github.com>
2019-07-12 10:13:05 +0200

    Update dockerExecuteOnKubernetes.groovy

--- vars/dockerExecuteOnKubernetes.groovy
+++ vars/dockerExecuteOnKubernetes.groovy
@@ -219,7 +219,7 @@ private String generatePodSpec(Map config) {
             containers: containers
         ]
     ]
-    podSpec.spec.securityContext = getSecurityContext(config)
+    //podSpec.spec.securityContext = getSecurityContext(config)
 
     return new JsonUtils().groovyObjectToPrettyJsonString(podSpec)
 }
